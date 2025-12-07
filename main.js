68a21b109524e6b59fc84563aa7de9bd57f64ae7
RafaInginMenjadiProgrammerHandal <rafa84434@gmail.com>
2025-12-07 16:55:20 +0700

    feat : age logic

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const prompt = require('prompt-sync')({ sigint: true });
 const nama = prompt('');
 const umur = prompt('');
+let uang = 0;
+
+if (umur < 21) {
+    console.log('Maaf ' + nama + ' umur kamu kurang ' + (21 - umur) + ' tahun lagi.');
+} else if (umur >= 21) {
+    uang = prompt('');
+} 
